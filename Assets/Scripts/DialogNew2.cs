e094ebfc5ac8e9a89b5096c229e0185a7f20471f
Jcastro26 <108453405+Jcastro26@users.noreply.github.com>
2022-09-29 17:30:24 -0500

    cambiosnuevosconsejero

---
+++ Assets/Scripts/DialogNew2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogNew2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*[SerializeField]  private GameObject DialogNew; //punto de referencia e identicar */
+    [SerializeField] private GameObject DialogText(TMP);
+    [SerializeField] private GameObject DialogPanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4,6)] private string [] dialogueLines; 
+
+   
+
+    // Update is called once per frame
+   /* void Update()
+    {
+        
+    }
+   
+    */
+}
