3176b719e05412e10273f60a60cae59b29bfd54b
Pab10Suarez <retroguias47@gmail.com>
2022-11-22 19:00:10 -0500

    Animacion funciones añadida

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -23,6 +23,13 @@ public class Graph : MonoBehaviour
     public float periodo;
     public TMP_InputField movhozfield;
     public float movhoz;
+    float next_spawn;
+    Vector3 position;
+    Vector3 scale;
+    float step;
+    Boolean oprimido;
+    int i;
+    Boolean entrargraf;
     // void Awake(){
     //     Graficar();
     // }
@@ -32,76 +39,99 @@ public class Graph : MonoBehaviour
     //     //Graficar(this.funcion);
     // }
     private void Start(){
+        
         //listener click
-        btnClick.onClick.AddListener(Graficar);
+        oprimido=false;
+        btnClick.onClick.AddListener(botonOprimido);
     }
-    public void GetInput(){
-        Debug.Log("Log input: "+amplitudfield.text);
-    } 
-    public void Graficar(){
-        //EditorUtility.DisplayDialog("a","Aguacate","sssas");  <-- dialogo de unity que se puede usar para avisar de errores al hacer input
-                //3f es el dominio de la funcion , la funcion se muestra de 
-        // si no se ha ingresado nada en el input field se pone en 1 por default        
+    void Update(){
+        
+        if(oprimido){
+            
+            Debug.Log("Hola de mar");
+            // si no se ha ingresado nada en el input field se pone en default
             if( amplitudfield.text==null ||amplitudfield.text==""){
-     
-          amplitudfield.text = "1";  
-          }
-          if( periodofield.text==null ||periodofield.text==""){
-     
-          periodofield.text = "1";  
-          }
-          if( movhozfield.text==null ||movhozfield.text==""){
-     
-          movhozfield.text = "0";  
-          }
-                
-                
+        
+                 amplitudfield.text = "1";  
+            }
+            if( periodofield.text==null ||periodofield.text==""){
+        
+                periodofield.text = "1";  
+            }
+            if( movhozfield.text==null ||movhozfield.text==""){
+        
+                movhozfield.text = "0";  
+            }
             this.amplitud=float.Parse(amplitudfield.text);      
             this.movhoz=float.Parse(movhozfield.text);    
             this.periodo=float.Parse(periodofield.text);
-        if(amplitud>=3){
-            resolution=900;
-        }
-        float step = dominio/resolution;
-        //vector.one= (1,1,1)
-        var scale = Vector3.one*step;
-        var position= Vector3.zero;
-        //cantidad de cubos
-        for (int i = 0; i<resolution;i++){
-            
-            Transform point=  Instantiate(PointPrefab);
-            //vector3.right mirando a la derecha (1,0,0) dividido pro 5 f para que se junten, +0.5 para llenar y -1f para moverlo en el eje x
-            //  si a step se le resta la mitad del dominio ej:-1.5f grafica a la izquierda y derecha del orgigen, sino grafica desde ese punto a la derecha
-            position.x = (i + 0.5f)*step ; 
-
-            // hace una funcion lineal f(x)=y=x 
-            //nota mental= aca se puede poder un metodo que modifique la funcion dependiendo de un parametro
-            switch(funcion){
-                case "seno":
-                    position.y= amplitud*seno(periodo*position.x);
-                    break;
-                case "coseno":
-                    position.y= coseno(position.x);
-                    break;
-                case "cuadratica":
-                    position.y= (( (cuadratica(position.x ) )/5)-1)*-1;
-                    break;
-                case "tangente":
-                    position.y= (( (tangente(position.x ) )));
-                    break;
+            if(amplitud>=3){
+                resolution=900;
             }
-   
-            //position.y=position.x; // funcion lineal y=x
-
-            //asigna el vector posicion a el punto 
-            point.localPosition = position;
-            //modifica la escala del punto *2f hace que sea mas grandes los cubos
-
-            point.localScale = scale*3f;
-            //hace que el padre sea el objeto graph (atra vez del atributo transform)
-            point.SetParent(transform,false);
- 
+            next_spawn=0;
+            entrargraf=true;
         }
+        while(entrargraf&&Time.time>next_spawn&&i<resolution){
+        Graficar();
+        i++;
+        }    
+        
+        oprimido=false;
+        
+    }
+
+    public  void Graficar(){
+        //EditorUtility.DisplayDialog("a","Aguacate","sssas");  <-- dialogo de unity que se puede usar para avisar de errores al hacer input
+                //3f es el dominio de la funcion , la funcion se muestra de 
+        Debug.Log("graficar llamada");        
+        
+        next_spawn=Time.time+1.0f;      
+         Debug.Log("Time. time= "+Time.time+"  next_spawn"+next_spawn+"  i = "+i);         
+        
+        //cantidad de cubos
+            Debug.Log("while entrao");
+            step = dominio/resolution;
+            var scale = new Vector3(1,1,1)*step;
+            var position= Vector3.zero; 
+        
+           
+            
+                Transform point=  Instantiate(PointPrefab);
+                //vector3.right mirando a la derecha (1,0,0) dividido pro 5 f para que se junten, +0.5 para llenar y -1f para moverlo en el eje x
+                //  si a step se le resta la mitad del dominio ej:-1.5f grafica a la izquierda y derecha del orgigen, sino grafica desde ese punto a la derecha
+                position.x = ((i) + 0.5f)*step ; 
+
+                // hace una funcion lineal f(x)=y=x 
+                //nota mental= aca se puede poder un metodo que modifique la funcion dependiendo de un parametro
+                switch(funcion){
+                    case "seno":
+                        position.y= amplitud*seno(periodo*position.x);
+                        break;
+                    case "coseno":
+                        position.y= coseno(position.x);
+                        break;
+                    case "cuadratica":
+                        position.y= (( (cuadratica(position.x ) )/5)-1)*-1;
+                        break;
+                    case "tangente":
+                        position.y= (( (tangente(position.x ) )));
+                        break;
+                }
+    
+                //position.y=position.x; // funcion lineal y=x
+
+                //asigna el vector posicion a el punto 
+                point.localPosition = position;
+                //modifica la escala del punto *2f hace que sea mas grandes los cubos
+
+                point.localScale = (scale)*3f;
+                //hace que el padre sea el objeto graph (atra vez del atributo transform)
+                point.SetParent(transform,false);
+                next_spawn=Time.time+0.01f;
+                
+        
+        
+    
     }
     //hacer objeto de cada uno con sus atributos
     public float seno(double x){
@@ -121,5 +151,7 @@ public class Graph : MonoBehaviour
     // radical raiz y mirar como tomar solo los valores positivos o algo pro el estilo 
     //exponencial falta agregar
     //logaritmica 
-
+    public void botonOprimido(){
+        oprimido=true;
+    }
 }
