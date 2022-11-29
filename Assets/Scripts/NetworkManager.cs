c91ea96e0e10b280b20ce337498f90d02e91c662
Pab10Suarez <retroguias47@gmail.com>
2022-11-29 07:54:19 -0500

    Firebase fucniones

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,31 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class NetworkManager : MonoBehaviour
-{
-public void CreateUser(string userName,string email,string pass, Action <Response> response)
- {
-  StartCoroutine( CO_CreateUser( userName , email , pass , response ) );
-}
-private IEnumerator CO_CreateUser(string userName,string email,string pass, Action <Response> response)//inicio de COrrutina para esperar al sevidor (por q no c hace en un frame)
- {
-   WWWForm form = new WWWForm();
-   form.AddField("username",userName);
-   form.AddField("email",email);
-   form.AddField("pass",pass);
-   WWW w = new WWW("http://localhost/Game/createuser.php",form);
+// public class NetworkManager : MonoBehaviour
+// {
+// public void CreateUser(string userName,string email,string pass, Action <Response> response)
+//  {
+//   StartCoroutine( CO_CreateUser( userName , email , pass , response ) );
+// }
+// private IEnumerator CO_CreateUser(string userName,string email,string pass, Action <Response> response)//inicio de COrrutina para esperar al sevidor (por q no c hace en un frame)
+//  {
+//    WWWForm form = new WWWForm();
+//    form.AddField("username",userName);
+//    form.AddField("email",email);
+//    form.AddField("pass",pass);
+//    WWW w = new WWW("http://localhost/Game/createuser.php",form);
 
-   yield return w;
+//    yield return w;
 
-   response (JsonUtility.FromJson<Response>(w.text) );//trasforma la respuesta en linea por una tipo response
-  }
+//    response (JsonUtility.FromJson<Response>(w.text) );//trasforma la respuesta en linea por una tipo response
+//   }
 
-}
- [Serializable]
-public class Response
-{
-    public bool done = false;
-    public string message = "usuario no registrado";
-}
+// }
+//  [Serializable]
+// public class Response
+// {
+//     public bool done = false;
+//     public string message = "usuario no registrado";
+// }
--- Assets/Scripts/SceneManager2.cs
+++ Assets/Scripts/SceneManager2.cs
@@ -5,46 +5,15 @@ using UnityEngine.UI;
 using TMPro;
 public class SceneManager2 : MonoBehaviour
 {
-     [SerializeField] public TMP_InputField m_userNameInput      = null;
-     [SerializeField] private TMP_InputField m_userEmailInput     = null;
-     [SerializeField] private TMP_InputField m_userPasswortInput  = null;
-     [SerializeField] private TMP_InputField m_userPasswort2Input = null;
-     [SerializeField] private Text       m_errorText          = null;
+
      [SerializeField] private GameObject m_registroUi          = null;
      [SerializeField] private GameObject m_LoginUI             = null;
 
-     private NetworkManager m_networkManager = null;
 
-     private void Awake()
-     {
-        m_networkManager = GameObject.FindObjectOfType<NetworkManager>();
-     }
-
-    
-     public void SubnitRegister(){
-    
-       if (m_userPasswortInput.text == m_userPasswort2Input.text)//si clave 1 es igual a clave 2
-      {
-          m_errorText.text="Procesando......";
-
-          m_networkManager.CreateUser(m_userNameInput.text,m_userEmailInput.text,m_userPasswortInput.text,delegate(Response response)//creando Usuario
-          {
-           m_errorText.text=response.message;
-          });
-        }
-      
-      else
-      {
-          m_errorText.text ="Error = contraseña no conside";
-      }
-     
-
-   }
-   
     public void ShowLogin()
     {
-     m_registroUi.SetActive(false);//para NO mostrar la ui Registro
-     m_LoginUI.SetActive(true);//para mostrar la ui de Login
+     m_registroUi.SetActive(false);//para  mostrar la ui Registro
+     m_LoginUI.SetActive(true);//para NOmostrar la ui de Login
      
      }
 
