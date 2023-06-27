4f638827953d22440ec58483e0c6638c332473b4
Andhec98 <136863857+Andhec98@users.noreply.github.com>
2023-06-27 03:45:44 -0500

    CASI

--- Assets/Scripts/Registro.cs
+++ Assets/Scripts/Registro.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class Registro : MonoBehaviour
 {
-    FirebaseFirestore db;
+    private Dictionary<string, string> registeredUsers;
+
     [SerializeField] public TMP_InputField registronombre = null;
     [SerializeField] private TMP_InputField registroEmail = null;
     [SerializeField] private TMP_InputField registroPass = null;
@@ -15,20 +17,70 @@ public class Registro : MonoBehaviour
     [SerializeField] public TMP_InputField loginnombre = null;
     [SerializeField] private TMP_InputField loginPass = null;
     [SerializeField] private Text loginerror = null;
+     [SerializeField] private GameObject m_LoginUI = null;
+     [SerializeField] private GameObject canvasmenu = null;
+     [SerializeField] private TMP_Text nombrejugador  = null;
+     [SerializeField] private TMP_Text iniciarsesionbotontexto  = null;
 
-    private CustomDictionary<string, object> usuarios;
+    private string currentUsername;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        usuarios = new CustomDictionary<string, object>();
-        
+        registeredUsers = new Dictionary<string, string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegisterUser()
+    {
+        REGISTROERROR.text="Procesando...";
+
+        if (registroPass.text == registroPass2.text)
+        {
+            if (!registeredUsers.ContainsKey("Nombre"))
+            {
+                registeredUsers.Add("Nombre", registronombre.text);
+                registeredUsers.Add("Correo", registroEmail.text);
+                registeredUsers.Add("Password", registroPass.text);
+
+                Debug.Log("Registro exitoso.");
+                REGISTROERROR.text = "¡Registro Completo! por favor inicie sesión";
+            }
+            else
+            {
+                REGISTROERROR.text = "El usuario ya existe.";
+            }
+        }
+        else
+        {
+            REGISTROERROR.text = "Error: la contraseña no coincide.";
+        }
+    }
+
+    public void LoginUser()
     {
         
+        
+        if (registeredUsers.ContainsKey("Nombre"))
+        {
+            if (registeredUsers["Nombre"].ToString() == loginnombre.text && registeredUsers["Password"].ToString() == loginPass.text)
+            {
+                currentUsername = registeredUsers["Nombre"].ToString();
+                Debug.Log("Inicio de sesión exitoso para el usuario: " + currentUsername);
+                nombrejugador.text="Bienvenido "+currentUsername;
+                //iniciarsesionbotontexto.text="Cambiar cuenta";
+                m_LoginUI.SetActive(false);
+                canvasmenu.SetActive(true);
+                Debug.Log("El nombre está en la base de datos, puedes volver al menu principal");
+                //Debug.Log("La contraseña está en la base de datos");
+            }
+            else {
+                loginerror.text="La contraseña ingresada es incorrecta";
+                Debug.Log("contraeña X");
+            }
+            
+        }
+        else
+        {
+            loginerror.text = "El usuario no existe.";
+        }
     }
 }
